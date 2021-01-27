11579f36e0e2a1547feec387db172a94a86d158b
click2cloud-theta <sumedha.game@click2cloud.net>
2021-01-27 11:46:49 +0530

    Update Documents for SSH Private Key

--- ecs/builder.hcl2spec.go
+++ ecs/builder.hcl2spec.go
@@ -107,7 +107,8 @@ type FlatConfig struct {
 	Type                                 *string                     `mapstructure:"communicator" cty:"communicator" hcl:"communicator"`
 	PauseBeforeConnect                   *string                     `mapstructure:"pause_before_connecting" cty:"pause_before_connecting" hcl:"pause_before_connecting"`
 
-	SSHPrivateIp *bool `mapstructure:"ssh_private_ip" required:"false" cty:"ssh_private_ip" hcl:"ssh_private_ip"`
+	SSHPrivateIp      *bool   `mapstructure:"ssh_private_ip" required:"false" cty:"ssh_private_ip" hcl:"ssh_private_ip"`
+	SSHPrivateKeyFile *string `mapstructure:"ssh_private_key_file" required:"false" cty:"ssh_private_key_file" hcl:"ssh_private_key_file"`
 }
 
 // FlatMapstructure returns a new FlatConfig.
@@ -176,6 +177,9 @@ func (*FlatConfig) HCL2Spec() map[string]hcldec.Spec {
 		"communicator":                 &hcldec.AttrSpec{Name: "communicator", Type: cty.String, Required: false},
 		"pause_before_connecting":      &hcldec.AttrSpec{Name: "pause_before_connecting", Type: cty.String, Required: false},
 		"ssh_private_ip":               &hcldec.AttrSpec{Name: "ssh_private_ip", Type: cty.Bool, Required: false},
+		"ssh_keypair_name":             &hcldec.AttrSpec{Name: "ssh_keypair_name", Type: cty.String, Required: false},
+		"ssh_private_key_file":         &hcldec.AttrSpec{Name: "ssh_private_key_file", Type: cty.String, Required: false},
+		"ssh_agent_auth":               &hcldec.AttrSpec{Name: "ssh_agent_auth", Type: cty.Bool, Required: false},
 	}
 	return s
 }
